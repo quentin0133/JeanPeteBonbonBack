d37cbc846eb512cc5b3065011d3f2c6054e2fb23
Quentin Yahia <relou0133@gmail.com>
2024-01-16 09:38:03 +0100

    Init jean-kasskouille project :tada:

---
+++ src/main/java/fr/dawan/jeankasskouille/JeanKasskouilleApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.jeankasskouille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeanKasskouilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeanKasskouilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/dawan/jeankasskouille/JeanKasskouilleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.jeankasskouille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeanKasskouilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
