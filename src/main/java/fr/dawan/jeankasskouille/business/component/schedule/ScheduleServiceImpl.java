c014ff0d706ebc75e9463805ac4dca17b346d50f
Quentin Yahia <relou0133@gmail.com>
2024-06-19 17:36:28 +0200

    feat : bot fixed, got bitches :beers:

--- src/main/java/fr/dawan/jeankasskouille/business/component/schedule/ScheduleServiceImpl.java
+++ src/main/java/fr/dawan/jeankasskouille/business/component/schedule/ScheduleServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 public class ScheduleServiceImpl extends GenericServiceImpl<Schedule, ScheduleRepository, ScheduleDto, ScheduleMapper> implements ScheduleService {
-    public ScheduleServiceImpl(ScheduleRepository repository, ScheduleMapper mapper, BotService botService) {
+    public ScheduleServiceImpl(ScheduleRepository repository, ScheduleMapper mapper) {
         super(repository, mapper);
     }
 }
